97e6c829c103feee5eb015fc3cf5e62cb17636d4
chrisb1986 <u1374769@unimail.hud.ac.uk>
2017-04-28 17:47:07 +0100

    Updated urls to display self generated pages from the browser url bar. Update views to enable this to happen

--- garage/urls.py
+++ garage/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /garage/ = garage index page.
     url(r'^$', views.index, name='index'),
+
+    # /garage/xx/ = garage id after /garage/ page.
+    url(r'^(?P<car_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- garage/views.py
+++ garage/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is 'Garage' page!</h1>")
+
+def detail(request, car_id):
+    return HttpResponse("<h2>Details for Car id: " + str(car_id) + "</h2>" )
