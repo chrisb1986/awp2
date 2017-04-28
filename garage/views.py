8b4421b9a0f74f3940333eb95a2a29dde16c6be7
chrisb1986 <u1374769@unimail.hud.ac.uk>
2017-04-28 18:30:18 +0100

    Update views to connect to db

--- garage/views.py
+++ garage/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from .models import Car_model
+
 
 def index(request):
-    return HttpResponse("<h1>This is 'Garage' page!</h1>")
+    all_cars  = Car_model.objects.all()
+    html = ''
+    for cars in all_cars:
+        url = '/garage/' + str(cars.id) + '/'
+        html += '<a href="' + url + '">' + cars.model_name + '</a><br>'
+    return HttpResponse(html)
+    # return HttpResponse("<h1>This is 'Garage' page!</h1>")
 
 def detail(request, car_id):
     return HttpResponse("<h2>Details for Car id: " + str(car_id) + "</h2>" )
