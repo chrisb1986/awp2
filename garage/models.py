26c1b3e2d1036ced24aa01fa53e1a1cb8cdbcca5
chrisb1986 <u1374769@unimail.hud.ac.uk>
2017-04-28 13:18:21 +0100

    Configured models

--- garage/models.py
+++ garage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Car_model(models.Model):
+    engine = models.CharField(max_length=4)
+    model_name = models.CharField(max_length=15)
+    colour = models.CharField(max_length=10)
+    car_image = models.CharField(max_length=1000)
+
+class Variation (models.Model):
+    car_model = models.ForeignKey(Car_model, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    variation_type = models.CharField(max_length=15)
