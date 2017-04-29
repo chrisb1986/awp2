cdd493cf0564aae8977b8fa7f57971ad33a84918
chrisb1986 <u1374769@unimail.hud.ac.uk>
2017-04-29 13:08:50 +0100

    Updated views template

--- garage/views.py
+++ garage/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.template import loader
+# from django.template import loader
+from django.shortcuts import render
 from .models import Car_model
+
 
 def index(request):
     all_cars  = Car_model.objects.all()
@@ -8,11 +10,10 @@ def index(request):
     # for cars in all_cars:
     #     url = '/garage/' + str(cars.id) + '/'
     #     html += '<a href="' + url + '">' + cars.model_name + '</a><br>'
-    template = loader.get_template('garage/index.html')
-    context = {
-        'all_cars': all_cars,
-    }
-    return HttpResponse(template.render(context, request))
+    # template = loader.get_template('garage/index.html')
+    context = {'all_cars': all_cars}
+    return render(request, 'garage/index.html', context)
+    # return HttpResponse(template.render(context, request))
     # return HttpResponse("<h1>This is 'Garage' page!</h1>")
 
 def detail(request, car_id):
