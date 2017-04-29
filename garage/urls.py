7f56f21aa889f47e66da21d1a5a07636f23988cb
chrisb1986 <u1374769@unimail.hud.ac.uk>
2017-04-29 13:27:58 +0100

    Updated views and urls for new detail template

--- garage/urls.py
+++ garage/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # /garage/xx/ = garage id after /garage/ page.
-    url(r'^(?P<car_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<car_id>[0-9]+)/$', views.detail, name='detail.html'),
 ]
--- garage/views.py
+++ garage/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.http import Http404
+# from django.http import HttpResponse
 # from django.template import loader
 from django.shortcuts import render
 from .models import Car_model
-
 
 def index(request):
     all_cars  = Car_model.objects.all()
@@ -12,9 +12,14 @@ def index(request):
     #     html += '<a href="' + url + '">' + cars.model_name + '</a><br>'
     # template = loader.get_template('garage/index.html')
     context = {'all_cars': all_cars}
-    return render(request, 'garage/index.html', context)
+    return render(request, 'garage/index.html', {'all_cars': all_cars})
     # return HttpResponse(template.render(context, request))
     # return HttpResponse("<h1>This is 'Garage' page!</h1>")
 
 def detail(request, car_id):
-    return HttpResponse("<h2>Details for Car id: " + str(car_id) + "</h2>" )
+    # return HttpResponse("<h2>Details for Car id: " + str(car_id) + "</h2>" )
+    try:
+        cars = Car_model.objects.get(pk=car_id)
+    except Car_model.DoesNotExist:
+        raise Http404("Car does not exist")
+    return render(request, 'garage/detail.html', {'cars': cars})
