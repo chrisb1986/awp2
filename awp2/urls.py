5cb47b252aa9f41e479c5872feccb426ef4a53ba
chrisb1986 <u1374769@unimail.hud.ac.uk>
2017-04-30 12:13:57 +0100

    Updated for file image upload

--- awp2/urls.py
+++ awp2/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -7,3 +9,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^garage/', include('garage.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
