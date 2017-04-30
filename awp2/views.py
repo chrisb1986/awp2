eedd44a308d0d9e86ae417d3fc8db6580d2a833f
chrisb1986 <u1374769@unimail.hud.ac.uk>
2017-04-30 15:33:49 +0100

    Updated everything. Site is now final! :-D

--- awp2/views.py
+++ awp2/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Index homepage!</h1>")
+    return HttpResponse("<h1>iGarage website created by Chris Brierley - U1374769</h1>"
+                        "<h2>Created using Django for Advance Web Programming Assignment #2</h2>"
+                        "<h3>Click <a href='/garage/'>HERE</a> to enter iGarage!</h3>")
